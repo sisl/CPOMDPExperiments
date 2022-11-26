1b92fdc14c568ad0f7a049ebba9a1096f37dceb3
Arec Jamgochian <jamgochian95@gmail.com>
2022-11-25 21:00:51 -0800

    moving over experiment files to appropriate folder

---
+++ experiments/find_good_spillpoint_settings copy.jl
@@ -0,0 +1,75 @@
+using Revise
+Pkg.resolve()
+using CPOMDPExperiments
+using D3Trees
+using Plots 
+
+### Find Good Settings
+kwargs = Dict(:tree_queries=>5e5, 
+        :k_observation => 0.1,
+        :alpha_action => 01/100,
+        :max_depth => 10,
+        :alpha_schedule => CPOMDPExperiments.CPOMCPOW.ConstantAlphaSchedule(1e-2))
+c = 250.0 # 250
+nu = 0.0
+λ_test = [1.]
+
+
+cpomdp = SoftConstraintPOMDPWrapper(CLightDarkNew(cost_budget=20.);λ=λ_test)
+solver = CPOMDPExperiments.CPOMCPOWSolver(;kwargs..., 
+    criterion=CPOMDPExperiments.CPOMCPOW.MaxCUCB(c, nu), 
+    estimate_value=zeroV_trueC,
+    tree_in_info=true,
+    search_progress_info=true)
+updater(planner) = CPOMDPExperiments.CPOMCPOW.CPOMCPOWBudgetUpdateWrapper(
+    CPOMDPExperiments.ParticleFilters.BootstrapFilter(cpomdp, Int(1e4), solver.rng), 
+    planner)
+hist3, R3, C3, RC3 = run_cpomdp_simulation(cpomdp, solver, updater)
+
+R3
+C3[1]
+RC3
+plot_lightdark_beliefs(hist3,"figs/belief_ldn_unconstrained.png")
+
+inchrome(D3Tree(hist3[1][:tree]))
+
+
+sp = SearchProgress(hist3[1])
+
+plot(sp.v_best)
+plot(sp.cv_best)
+plot(sp.v_taken)
+plot(sp.cv_taken)
+plot(sp.lambda)
+
+
+## constrained
+
+cpomdp = SoftConstraintPOMDPWrapper(CLightDarkNew(cost_budget=0.5);λ=λ_test)
+solver = CPOMDPExperiments.CPOMCPOWSolver(;kwargs..., 
+    criterion=CPOMDPExperiments.CPOMCPOW.MaxCUCB(c, nu), 
+    estimate_value=zeroV_trueC,
+    tree_in_info=true,
+    search_progress_info=true)
+updater(planner) = CPOMDPExperiments.CPOMCPOW.CPOMCPOWBudgetUpdateWrapper(
+    CPOMDPExperiments.ParticleFilters.BootstrapFilter(cpomdp, Int(1e4), solver.rng), 
+    planner)
+hist4, R4, C4, RC4 = run_cpomdp_simulation(cpomdp, solver, updater)
+
+R4
+C4[1]
+RC4
+plot_lightdark_beliefs(hist4,"figs/belief_ldn_constrained.png")
+
+inchrome(D3Tree(hist4[1][:tree]))
+
+
+sp = SearchProgress(hist4[1])
+
+plot(sp.v_best)
+plot(sp.cv_best)
+plot(sp.v_taken)
+plot(sp.cv_taken)
+plot(sp.lambda)
+
+
